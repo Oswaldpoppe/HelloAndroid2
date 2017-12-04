c4eadbd190758cd0ce172bffeb7bc590008fc89a
Oswald <poppe.oswald@gmail.com>
2017-12-04 15:11:39 +0100

    final

---
+++ app/src/main/java/com/oswald/android/helloandroid/HelloAndroid.java
@@ -0,0 +1,19 @@
+package com.oswald.android.helloandroid;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class HelloAndroid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_android);
+
+
+        };
+    }
