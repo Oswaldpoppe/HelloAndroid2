36cf3ff85847817c6a0d606e703d612113a906b0
Oswald <poppe.oswald@gmail.com>
2018-02-19 12:33:06 +0100

    final

--- app/src/main/java/com/oswald/android/helloandroid/HelloAndroid.java
+++ app/src/main/java/com/oswald/android/helloandroid/HelloAndroid.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class HelloAndroid extends AppCompatActivity {
 
     @Override
@@ -15,5 +17,26 @@ public class HelloAndroid extends AppCompatActivity {
         setContentView(R.layout.activity_hello_android);
 
 
-        };
     }
+
+    public void maps(View view) {
+        String map = "http://maps.google.co.in/maps?q=" + getString(R.string.address);
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(map));
+        startActivity(intent);
+    }
+
+    /**
+     * This method is called when the contact us button is clicked.
+     */
+    public void contactUs(View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setType("message/rfc822");
+        intent.setData(Uri.parse(getString(R.string.mail_address)));
+        intent.putExtra(intent.EXTRA_SUBJECT, getString(R.string.subject));
+        intent.putExtra(intent.EXTRA_TEXT, getString(R.string.mail_content));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+}
+
